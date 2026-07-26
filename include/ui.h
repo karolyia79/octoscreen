d6e2a0d927296225b1a4150f5c540f08ba5934ac
karolyia79 <87782660+karolyia79@users.noreply.github.com>
2026-07-26 15:01:08 +0200

    Create ui.h

---
+++ include/ui.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <Arduino.h>
+
+void updateTheme();
+void updateLEDAnimation();
+void drawStatusScreen();
+void drawTuningMenu();
+void drawMainMenu();
+void drawSettingsMenu();
